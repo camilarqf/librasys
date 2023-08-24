ba60cde2f4a52b72726502eec305ecd78cf1b5ec
Camila Fonseca <camilarqf@gmail.com>
2023-08-24 20:14:13 -0300

    first commit

---
+++ src/main/java/br/com/librasys/LibrasysApplication.java
@@ -0,0 +1,13 @@
+package br.com.librasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrasysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/librasys/LibrasysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.librasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
